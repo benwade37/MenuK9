c3f1f10a634f0b86f98a3062f9245ffeb2a92602
Ben <benwade37@me.com>
2025-09-25 12:43:14 +0100

    Placeholder text added

--- recipes/forms.py
+++ recipes/forms.py
@@ -5,7 +5,21 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'steps', 'image']  # Include new fields
-
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'For example: The Guvnors Gravy'
+                }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'rows': 3,
+                'placeholder': 'For example: This is a gravy recipe tweaked for dogs, packed with vitamins for a healthy coat and joints'
+                }),
+            'steps': forms.Textarea(attrs={
+                'class': 'form-control', 
+                'rows': 10,
+                'placeholder': 'Step 1: Take out small bones and skin \nStep 2: Mix ingredients \nStep 3: Simmer for 2 hours \nEtc'}),
+        }
     def clean_image(self):
         image = self.cleaned_data.get('image')
         if image and image.size > 5 * 1024 * 1024:  # 5 MB limit
--- recipes/models.py
+++ recipes/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class Recipe(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    steps = models.TextField(default="No steps provided.")  # New field for recipe steps  # New field for recipe steps
+    steps = models.TextField()  # New field for recipe steps  # New field for recipe steps
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

e84b1077d0278fe4beb41ac546fd88dcedc2bbe4
Ben <benwade37@me.com>
2025-10-02 16:02:09 +0100

    Remove env.py from repository

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_e2OJN1wyZSML@ep-little-sun-a2ty3h11.eu-central-1.aws.neon.tech/skate_bring_cash_870254")
-os.environ.setdefault("DEBUG", "False")  # Set to "True" for development
-os.environ.setdefault("CLOUDINARY_CLOUD_NAME", "dezh8szkv")
-os.environ.setdefault("CLOUDINARY_API_KEY", "485488141619233")
-os.environ.setdefault("CLOUDINARY_API_SECRET", "PLRHGRPyVYsWIxUhfbFc5z8fEKM")
-os.environ.setdefault("ALLOWED_HOSTS", "127.0.0.1,localhost,menuk9-app.herokuapp.com")
